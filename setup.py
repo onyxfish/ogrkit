c166c87ab20c6867ba607563071ed2e99eabfd6c
Christopher Groskopf <staringmonkey@gmail.com>
2011-07-29 17:08:10 -0500

    Revved version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ogrkit',
-    version='0.0.1',
+    version='0.0.2',
     description='A suite of command-line utilities implementing basic geometric operations. ',
     long_description=open('README.rst').read(),
     author='Christopher Groskopf',
@@ -17,6 +17,7 @@ setup(
     ],
     scripts = [
         'ogrdifference',
+        'ogrintersection'
     ],
     install_requires = [
         'GDAL==1.6.0']
